899132a4b7de05cfcf1d5b34160ce169386399a9
Arne Seemann <arne.seemann@gmail.com>
2016-02-10 16:07:00 +0100

    Style post date

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -83,16 +83,18 @@ function echoplex_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'echoplex' ),
+		esc_html_x( '%s', 'post date', 'echoplex' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'echoplex' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
+	// $byline = sprintf(
+	// 	esc_html_x( 'by %s', 'post author', 'echoplex' ),
+	// 	'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	// );
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	// echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+
+	echo '<span class="posted-on">' . $time_string . '</span>';
 
 }
 endif;
